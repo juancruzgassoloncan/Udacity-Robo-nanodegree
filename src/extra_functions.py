ede972573750b860633c92ce6c021f98d05e94e7
Juan Cruz Gasso Loncan <juancruzgassoloncan@gmail.com>
2017-10-04 15:04:26 -0300

    ex_3

--- src/extra_functions.py
+++ src/extra_functions.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import matplotlib.image as mpimg
 
+
 def perspect_transform(img, src, dst):
 
     # Get transform matrix using cv2.getPerspectivTransform()
@@ -12,21 +13,24 @@ def perspect_transform(img, src, dst):
     # Return the result
     return warped
 
+
 def color_thresh(img, rgb_thresh=(160, 160, 160)):
     # Create an array of zeros same xy size as img, but single channel
-    color_select = np.zeros_like(img[:,:,0])
+    color_select = np.zeros_like(img[:, :, 0])
     # Require that each pixel be above all thre threshold values in RGB
     # above_thresh will now contain a boolean array with "True"
     # where threshold was met
-    above_thresh = (img[:,:,0] > rgb_thresh[0]) \
-                & (img[:,:,1] > rgb_thresh[1]) \
-                & (img[:,:,2] > rgb_thresh[2])
+    above_thresh = (img[:, :, 0] > rgb_thresh[0]) \
+        & (img[:, :, 1] > rgb_thresh[1]) \
+        & (img[:, :, 2] > rgb_thresh[2])
     # Index the array of zeros with the boolean array and set to 1
     color_select[above_thresh] = 1
     # Return the binary image
     return color_select
 
-image = mpimg.imread('sample.jpg')
+
+image_name = '../data/IMG/robocam_2017_10_03_15_35_32_475.jpg'
+image = mpimg.imread(image_name)
 # Define calibration box in source (actual) and destination (desired) coordinates
 # These source and destination points are defined to warp the image
 # to a grid where each 10x10 pixel square represents 1 square meter
@@ -34,9 +38,10 @@ dst_size = 5
 # Set a bottom offset to account for the fact that the bottom of the image
 # is not the position of the rover but a bit in front of it
 bottom_offset = 6
-source = np.float32([[14, 140], [301 ,140],[200, 96], [118, 96]])
-destination = np.float32([[image.shape[1]/2 - dst_size, image.shape[0] - bottom_offset],
-                  [image.shape[1]/2 + dst_size, image.shape[0] - bottom_offset],
-                  [image.shape[1]/2 + dst_size, image.shape[0] - 2*dst_size - bottom_offset],
-                  [image.shape[1]/2 - dst_size, image.shape[0] - 2*dst_size - bottom_offset],
-                  ])
+source = np.float32([[35, 135], [120, 97], [202, 97], [300, 135]])
+destination = np.float32([[image.shape[1] / 2 - dst_size, image.shape[0] - bottom_offset],
+                          [image.shape[1] / 2 - dst_size, image.shape[0] -
+                              bottom_offset - 2 * dst_size],
+                          [image.shape[1] / 2 + dst_size, image.shape[0] -
+                              bottom_offset - 2 * dst_size],
+                          [image.shape[1] / 2 + dst_size, image.shape[0] - bottom_offset]])
--- src/rover_cords.py
+++ src/rover_cords.py
@@ -1,20 +1,23 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import numpy as np
+import numpy as nps
 import cv2
 from extra_functions import perspect_transform, color_thresh, source, destination
 
 # Read in the sample image
-image = mpimg.imread('sample.jpg')
+image_name = '../data/IMG/robocam_2017_10_03_15_35_32_475.jpg'
+image = mpimg.imread(image_name)
 
 
 def rover_coords(binary_img):
     # TODO: fill in this function to
     # Calculate pixel positions with reference to the rover
     # position being at the center bottom of the image.
-    y_pixel = 0
-    x_pixel = 0
+    yb, xb = binary_img.nonzero()
+    y_pixel = -(xb - binary_img.shape[0])
+    x_pixel = -(yb - binary_img.shape[1] / 2)
     return x_pixel, y_pixel
+
 
 # Perform warping and color thresholding
 warped = perspect_transform(image, source, destination)
@@ -29,4 +32,4 @@ plt.plot(xpix, ypix, '.')
 plt.ylim(-160, 160)
 plt.xlim(0, 160)
 plt.title('Rover-Centric Map', fontsize=20)
-#plt.show() # Uncomment if running on your local machine
+plt.show()  # Uncomment if running on your local machine
