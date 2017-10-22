dea9f8e401957e9e5c08d6f5e50d271bea0d2b64
Juan Cruz Gasso Loncan <juancruzgassoloncan@gmail.com>
2017-10-22 12:41:28 -0300

    first catkin workspace

---
+++ ros-ws/1-catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /media/juan/Datos/Repositorios/Udacity-Robo-nanodegree/ros-ws/1-catkin_ws/src
+# Build directory: /media/juan/Datos/Repositorios/Udacity-Robo-nanodegree/ros-ws/1-catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
